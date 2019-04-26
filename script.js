4ad37b894c578010fcdfa1a8076c4e6be3b9b1ef
Alexandre VOGEL <alexvogel2000@gmail.com>
2019-04-26 14:55:42 +0200

    ajout chargement automatique

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 var tab = [];
 
-$(document).ready(function () {
-
-  class ListItem {
-    constructor(mes, disp) {
-      if (typeof(mes) === 'string') {
-          this.mes = mes;
-      }
-      else {
-        console.error('mes not string');
-      }
-      if (typeof(disp) === 'boolean') {
-        this.disp = disp;
-      }
-      else {
-        console.error('disp not bool');
-      };
+class ListItem {
+  constructor(mes, disp) {
+    if (typeof(mes) === 'string') {
+        this.mes = mes;
+    }
+    else {
+      console.error('mes not string');
+    }
+    if (typeof(disp) === 'boolean') {
+      this.disp = disp;
+    }
+    else {
+      console.error('disp not bool');
     };
   };
+};
+
+function load() {
+  $('li').remove();
+  tab=JSON.parse(localStorage.getItem('tab'));
+
+  $.each(tab, function (index, obj) {
+    if (obj.disp == true) {
+      $('ul').append("<li class=\"notclique\">"+obj.mes+"</li>");
+    }
+    else {
+      $('ul').append("<li class=\"clique\">"+obj.mes+"</li>");
+    }
+  });
+};
+
+$(document).ready(function () {
 
   $('#add_todo').click(function () {
     $('ul').append("<li class=\"notclique\">"+$('#name_of_todo').val()+"</li>");
@@ -29,36 +43,34 @@ $(document).ready(function () {
     $(this).toggleClass("clique");
   });
 
-  $('#save').click( function () {
-    tab = [];
-    $('li').each(function(){
-      if($(this).hasClass('clique')){
-        tab.push(new ListItem($(this).text(), false));
-      }
-      else {
-        tab.push(new ListItem($(this).text(), true));
-      }
+  if(typeof(Storage) !== "undefined") {
+
+    if (localStorage.getItem('tab') != null) {
+      load();
+    }
+    
+    $('#save').click( function () {
+      tab = [];
+      $('li').each(function(){
+        if($(this).hasClass('clique')){
+          tab.push(new ListItem($(this).text(), false));
+        }
+        else {
+          tab.push(new ListItem($(this).text(), true));
+        }
+      });
+      localStorage.setItem('tab', JSON.stringify(tab));
     });
-    localStorage.setItem('tab', JSON.stringify(tab));
-  });
 
-  $('#load').click( function () {
-    $('li').remove();
-    tab=JSON.parse(localStorage.getItem('tab'));
+    $('#load').click(load());
 
-    $.each(tab, function (index, obj) {
-      if (obj.disp == true) {
-        $('ul').append("<li class=\"notclique\">"+obj.mes+"</li>");
-      }
-      else {
-        $('ul').append("<li class=\"clique\">"+obj.mes+"</li>");
-      };
+    $('#rm').click( function () {
+      localStorage.removeItem('tab');
+        $('li').remove();
     });
-  });
-
-  $('#rm').click( function () {
-    localStorage.setItem('tab', null);
-      $('li').remove();
-  });
+  }
+  else {
+    alert("Malheureusement votre navigateur ne permet pas de sauvegarder vos todo");
+  }
   
 });
